118fd54c0bfdd653043a30712d7b6801bcf41b37
gr1ffix <grffin.stafford@mail.ru>
2021-09-21 20:51:57 +0300

    чистка кода

--- 21.09/Startup.cs
+++ 21.09/Startup.cs
@@ -12,20 +12,12 @@ namespace _21._09
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+ 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
